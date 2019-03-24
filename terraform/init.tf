1fe49578f089ede1d97ccf8196292fe0250f0550
Unknown <ebrima.jobe92@gmail.com>
2019-03-24 18:47:03 -0400

    fix key pair issues

--- terraform/init.tf
+++ terraform/init.tf
@@ -1,5 +1,6 @@
 variable "access_key" {}
 variable "secret_key" {}
+variable "public_key" {}
 
 provider "aws" {
   access_key = "${var.access_key}"
@@ -10,9 +11,9 @@ provider "aws" {
 
 resource "aws_key_pair" "freshly" {
   key_name   = "freshly"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKtoh3+IeDavWgUatmgLtrXrdOunMUJwEIhHzwYtygCFtZGFchuFLaAK3qeS5M1FQlmaVZoY62HI6PZ+7so1uwD85alsPCopmUqJj09FTJK/2wESfdcWGD9c9fZqoeEOcV0UkuvUX/GW6CaPQ8DZQ5vPazNm+uId0KTlsu9Qu75KCI0WSCYPOGCUASOrsh4qvySAYHOygYoFf5bywCnBxwS0zU/vUBlNV+tOlIfl1AZ0NQ0DsY4rwSBzcgafsOCMeKeKb03ANogGGi3hb3s4VD2khmou0vYm4QrdS9LME64uCnJt2//Yld0ouIs3gyCsiAAR7e2qcTyuxCgtvXx82h macbookpro@freshlys-MacBook-Pro.local"
+  public_key = "${var.public_key}"
 }
-resource "aws_instance" "example" {
+resource "aws_instance" "crank" {
   ami           = "ami-035be7bafff33b6b6"
   instance_type = "t2.micro"
   key_name = "freshly"
@@ -25,7 +26,7 @@ resource "aws_instance" "example" {
 # }
 
 resource "aws_eip" "crank" {
-  instance = "${aws_instance.example.id}"
+  instance = "${aws_instance.crank.id}"
   vpc      = true
 }
 output "ip_addy" {
