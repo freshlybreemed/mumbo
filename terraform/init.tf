bbd6f41cce67c33c5a2e23e9d4f56d0104c73ca9
Unknown <ebrima.jobe92@gmail.com>
2019-03-23 22:59:49 -0400

    Testing eip

--- terraform/init.tf
+++ terraform/init.tf
@@ -11,7 +11,7 @@ resource "aws_key_pair" "freshly" {
   key_name   = "freshly"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKtoh3+IeDavWgUatmgLtrXrdOunMUJwEIhHzwYtygCFtZGFchuFLaAK3qeS5M1FQlmaVZoY62HI6PZ+7so1uwD85alsPCopmUqJj09FTJK/2wESfdcWGD9c9fZqoeEOcV0UkuvUX/GW6CaPQ8DZQ5vPazNm+uId0KTlsu9Qu75KCI0WSCYPOGCUASOrsh4qvySAYHOygYoFf5bywCnBxwS0zU/vUBlNV+tOlIfl1AZ0NQ0DsY4rwSBzcgafsOCMeKeKb03ANogGGi3hb3s4VD2khmou0vYm4QrdS9LME64uCnJt2//Yld0ouIs3gyCsiAAR7e2qcTyuxCgtvXx82h macbookpro@freshlys-MacBook-Pro.local"
 }
-resource "aws_instance" "example" {
+resource "aws_instance" "web" {
   ami           = "ami-035be7bafff33b6b6"
   instance_type = "t2.micro"
   key_name = "freshly"
@@ -23,6 +23,10 @@ resource "aws_instance" "example" {
 #   acl = "private"
 # }
 
+resource "aws_eip" "crank" {
+  instance = "${aws_instance.web.id}"
+  vpc      = true
+}
 output "ip_addy" {
   value = "${aws_instance.example.public_dns}"
 }
