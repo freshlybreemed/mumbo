aaeab910adb9faaae113e6fde8a96cf64fbc599c
Unknown <ebrima.jobe92@gmail.com>
2019-03-27 23:03:45 -0400

    Gif + about section

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,9 @@ import { Content } from 'nextein/post'
 export default withPosts( ({ posts }) => {
   return (
     <div class="wrapper">
+     <div class="row">
+        <div class="lineup">CHICKEN & MUMBO SAUCE PRESENTS</div><br></br>
+      </div>
       <img src="static/img/logocrank.png" class="flyerimage" />
       <img src="static/img/cash.png" class="cash"/>
       <div class="features"> 
@@ -31,31 +34,41 @@ export default withPosts( ({ posts }) => {
         <div class="date">9PM - 2AM</div>
       </div>
       <div class="row">
-        <div class="lineup">2002 FENWICK ST NW DC </div>
+        <div class="lineup">2002 FENWICK ST NW DC 20002</div>
       </div>
       <div class="row">
       <br></br>
+      <div class="row">
+        <div class="tickets">ABOUT</div><br></br>
+      </div> 
+      <div class="row">
+        <div class="about">
+          Crank Karaoke is a one of a kind, improv jam session and open mic where the funky sounds of go-go meet local musicians, singers, songwriters, rappers, poets, dancers and more to experience music together and express themselves freely. Featuring a rotating house band of certified local musical talent, look forward to a karaoke-inspired experience with a not-so-shy audience singing popular covers and renditions of culturally relevant songs. Essentially, it’s a kickback where beginners and experts can share the stage to learn and create together. We know the power of music and we want to use it to restore not exploit.
+        </div>
+        <br></br>
+        <img src="static/img/onstage.gif"/>
+      </div>
       <div class="row">
         <div class="tickets">TICKETS</div><br></br>
       </div> 
         <form action="https://chickenandmumbosauce.us13.list-manage.com/subscribe/post?u=ed6d4b4a06108687ea3bb3473&amp;id=66bc11430c" method="post" class="rsvp-form">
           <div class="rsvp-form" >
             <label for="name">First Name: </label>
-            <input type="text" name="FNAME" id="name" placeholder="Name" required/>
+            <input type="text" name="FNAME" id="name" required/>
           </div>
           <br></br>
           <div class="rsvp-form">
             <label for="name">Last Name: </label>
-            <input type="text" name="LNAME" id="name" placeholder="Name" required/>
+            <input type="text" name="LNAME" id="name" required/>
           </div>
           <br></br>
           <div class="rsvp-form">
             <label for="email">Email Address: </label>
-            <input type="email" name="EMAIL" id="email" placeholder="Email" required/>
+            <input type="email" name="EMAIL" id="email" required/>
           </div>
           <br></br>
           <div class="rsvp-form">
-            <input type="submit" value="RSVP!"/>
+            <input type="submit" value="RSVP"/>
           </div>
         </form>
       {/* <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css"/> */}
