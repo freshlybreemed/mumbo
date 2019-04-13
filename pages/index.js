c5c5738449193087411513954422e7f5219db0f7
Unknown <ebrima.jobe92@gmail.com>
2019-04-13 17:03:22 -0400

    RECAP MODE

--- pages/index.js
+++ pages/index.js
@@ -11,21 +11,9 @@ export default withPosts( ({ posts }) => {
         <div class="lineup">CHICKEN & MUMBO SAUCE PRESENTS</div><br></br>
       </div>
       <img src="static/img/logocrank.png" class="flyerimage" />
-      <img src="static/img/cash.gif" class="cash"/>
-      <div class="features"> 
-        <div class="item">LIVE BAND</div> 
-        <img class="yellow-star" src="static/img/yellowstar.png"></img>
-        <div class="item">GO-GO KARAKOKE </div>
-        <img class="yellow-star" src="static/img/yellowstar.png"></img>
-        <div class="item">JAM SESSION</div>
-      </div>
-      <div class="row">
-        <div class="title">BIG CHIEF</div><br></br>
-      </div>
-      <div class="row">
-        <div class="lineup">HOSTED BY: WALK LIKE WALT</div>
-      </div>
-      <div class="row">
+      
+ 
+      {/* <div class="row">
         <div class="lineup">DJ SETS: MALCOLM XAVIER + FRESHLY BREEMED</div>
       </div>
       <div class="row">
@@ -33,54 +21,42 @@ export default withPosts( ({ posts }) => {
       </div>
       <div class="row">
         <div class="date">9PM - 2AM</div>
-      </div>
-      <div class="row">
-        <div class="lineup">2002 FENWICK ST NE DC 20002</div>
-      </div>
+      </div> */}
       <div class="row">
       <br></br>
-      <div class="row">
+      {/* <div class="row">
         <div class="tickets"><img class="yellow-star" src="static/img/yellowstar.png"/>
               THIS IS YOUR NIGHT 
           TO BE LEAD MIC
           <img class="yellow-star" src="static/img/yellowstar.png"/>
         </div>
-      </div> 
+      </div>  */}
       <div class="row">
         <div class="about">
-        Crank Karaoke is a one of a kind, improv jam session and open mic where the funky sounds of go-go collide with community to experience music together and express themselves freely. Featuring a live band of former go-go band musicians, look forward to a karaoke-inspired experience with a not-so-shy audience singing popular covers and renditions of culturally relevant songs. Essentially, it’s a party where beginners and experts can share the stage to learn and create together. We know the power of go-go music and we want to use it to restore the culture not exploit it.        </div>
+          First and foremost we’d like to thank the people for showing up and showing out last night! We stood and rocked out in solidarity as we shook the city up with #crankkaraoke. You can’t replicate it or mute DC. We R One and we’re here to stay. 
+        </div>
         <br></br>
-        <img class="gif2" src="static/img/onstage.gif"/>
+        <div class="rsvp-form">
+          <form action="/recap">
+              <input type="submit" value="PHOTO RECAP" />
+          </form>
+      </div>
+
         {/* Its not a hit until a go-go band remakes it! */}
       </div>
       <br></br>
       <div class="row">
-        <div class="tickets">SIGN UP NOW</div><br></br>
+        <div class="tickets">MISSED THE FIRST ONE? <br></br>SIGN UP NOW FOR THE NEXT ONE</div><br></br>
       </div> 
-        <form action="https://chickenandmumbosauce.us13.list-manage.com/subscribe/post?u=ed6d4b4a06108687ea3bb3473&amp;id=66bc11430c" method="post" class="rsvp-form">
-          <div class="rsvp-form" >
-            <label for="name">First Name: </label>
-            <input type="text" name="FNAME" id="name" required/>
-          </div>
-          <br></br>
-          <div class="rsvp-form">
-            <label for="name">Last Name: </label>
-            <input type="text" name="LNAME" id="name" required/>
-          </div>
-          <br></br>
+        <form action="https://chickenandmumbosauce.us13.list-manage.com/subscribe/post?u=ed6d4b4a06108687ea3bb3473&amp;id=a4855a812b" method="post" class="rsvp-form">
           <div class="rsvp-form">
             <label for="email">Email Address: </label>
             <input type="email" name="EMAIL" id="email" required/>
           </div>
           <br></br>
-          <div class="rsvp-form">
-            <label for="song">Favorite Song: </label>
-            <input type="text" name="SONG" id="song"/>
-          </div>
-          <br></br>
           <br></br>
           <div class="rsvp-form">
-            <input type="submit" value="RSVP"/>
+            <input type="submit" value="STAMP"/>
           </div>
         </form>
       {/* <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css"/> */}
