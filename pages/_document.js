7828f7dd7360a5952be671ea2e57fa23f343bb28
Unknown <ebrima.jobe92@gmail.com>
2019-04-21 17:49:59 -0400

    Testing post-merge

--- pages/_document.js
+++ pages/_document.js
@@ -26,14 +26,6 @@ export default class MyDocument extends Document {
                   
                   <NextScript />
               </body>
-              <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138253949-1"></script>
-              <script>
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', 'UA-138253949-1');
-                </script>
             </html>
         )
     }
--- pages/recap.js
+++ pages/recap.js
@@ -5,7 +5,6 @@ import withPost from 'nextein/post'
 export default withPost( ({ post }) => { 
     return(
         <div class="wrapper">
-
             <div class="row"> 
                 <img src="static/img/logocrank.png" class="cash"/>
                 {/* <div class="row">
